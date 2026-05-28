6e6a06a74ddc030b05e347bebf7632ca2cd0af87
Rajani <rajanirb11@gmail.com>
2026-05-28 12:09:30 +0530

    Prepared backend for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,3 +138,8 @@ REST_FRAMEWORK = {
     ),
 
 }
+ALLOWED_HOSTS = ['*']
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
